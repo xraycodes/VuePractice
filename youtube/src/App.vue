eda58a04f86f1160fb0922bf2586b56fd9793415
brianjo94 <brianjo1994@gmail.com>
2023-12-05 22:23:38 -0500

    left and top navbar started. Used flexbox

---
+++ youtube/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+    import {} from 'vue'
+    
+    import LeftNavbar from './components/LeftNavbar.vue'
+
+</script>
+
+<template>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="flexbox-container">
+        <img src="./assets/images/YTLogo.png" width="70" height="40">
+    <input type="text" placeholder="Search">
+    <button>Upload</button>
+    <button>Bell</button>
+    </div>
+</body>
+
+<LeftNavbar/>
+</html>
+
+</template>
+
+<style scoped>
+.flexbox-container {
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+    align-content: flex-start;
+    margin: 0 0 20px 0
+    /* background-color: aqua; */
+}
+
+</style>
---
+++ youtube/src/components/LeftNavbar.vue
@@ -0,0 +1,47 @@
+<script setup>
+const topMenu = ['Home', 'Shorts', 'Subscriptions']
+const middleMenu = ['Your channel','History','Your videos', 'Watch later','Show more']
+const lowerMenu = ['Subscriptions']
+
+</script>
+
+<template>
+    <div id="outer-container">
+        <div id="topMenu">
+            <div v-for="menu in topMenu">{{ menu }}</div>
+        </div>
+        <hr>
+
+        <div id="middleMenu">
+            <div v-for="menu in middleMenu">{{ menu }}</div>
+        </div>
+    
+    
+
+    </div>
+
+
+</template>
+
+<style>
+
+#outer-container {
+    width: 15%;
+    background-color: pink;
+}
+#topMenu {
+    display:flex;
+    flex-direction: column;
+    /* justify-content: center; */
+    align-items: center;
+    /* background-color: red;; */
+
+}
+
+#middleMenu {
+    display: flex;
+    flex-direction:column;
+    align-items: center;
+}
+
+</style>
